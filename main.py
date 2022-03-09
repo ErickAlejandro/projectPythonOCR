78fbeb44f1aa241530905fcde152ab5d9879b8df
ErickAlejandro <acea1234@hotmail.com>
2022-03-09 11:58:43 -0500

    added data totals

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import cv2
 import pytesseract as pt
 
-img = cv2.imread('nombre.png')
+img = cv2.imread('/home/foranito/learningPython/projectPython/cedulas_transformadas/C27.png')
 # img = str(img)
 # print(img)
 # Adding custom options
 custom_config = r'--oem 3 --psm 6'
 img_t = pt.image_to_string(img, config=custom_config)
 print(str(img_t))
+
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
